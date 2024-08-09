42f6b241547be3c4908c0ba28011aa64dd3875ed
Dedipya1052 <149998158+Dedipya1052@users.noreply.github.com>
2024-08-09 19:04:32 +0530

    Add files via upload

---
+++ Bubble_sort(aug6).c
@@ -0,0 +1,31 @@
+//Bubble Sort
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+	int a[20],i,j,temp,n;
+	printf("Enter number of elements you want to input : ");
+	scanf("%d",&n);
+	printf("Enter numbers");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=n-1;i>=1;i--)
+	{
+		for(j=0;j<i;j++)
+		{
+			if(a[j]>a[j+1])
+			{
+				temp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
+			}
+		}
+	}
+	printf("Values after Bubble sort");
+			for(i=0;i<n;i++){
+			
+			printf("%d ",a[i]);
+		}
+	
+}
---
+++ Selection_sort(Aug6).c
@@ -0,0 +1,38 @@
+//Selection Sort
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+	int a[20],i,j,temp,n,index,min;
+	printf("Enter number of elements you want to input : ");
+	scanf("%d",&n);
+	printf("Enter numbers");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for (i=0;i<n-1;i++){
+		min=a[i];
+		index=i;
+		for(j=i+1;j<n;j++)
+		{
+			if(a[j]<min) //descending order logic= if(a[j]>min)
+			{
+				min=a[j];
+				index=j;
+		}
+	}
+	if(index!=i)
+	{
+		temp=a[i];
+		a[i]=a[index];
+		a[index]=temp;
+	}
+			}
+			printf("Values after selection sort");
+			for(i=0;i<n;i++){
+			
+			printf("%d ",a[i]);
+		}
+		}
+	
+
