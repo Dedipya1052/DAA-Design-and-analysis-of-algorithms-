49b68fa60dff9bb7164c0a420baea639878d51b8
Dedipya1052 <149998158+Dedipya1052@users.noreply.github.com>
2024-08-11 19:22:54 +0530

    Update and rename Insertion_sort(Aug6).c to Insertion_Sort(Aug6).c

--- Insertion_Sort(Aug6).c
+++ Insertion_Sort(Aug6).c
@@ -5,7 +5,7 @@ int main(){
 	int a[20],i,j,temp,n,key,k;
 	printf("Enter number of elements you want to input : ");
 	scanf("%d",&n);
-	printf("Enter numbers");
+	printf("Enter numbers : ");
 	for(i=0;i<n;i++)
 	{
 		scanf("%d",&a[i]);
